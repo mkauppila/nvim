9270f403de16824a46818f5b2c3b87811eade5c4
Markus Kauppila <markus.kauppila@gmail.com>
2023-06-03 12:50:31 +0300

    Execute Packer Install automatically on startup

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
 require('plugins')
 
+vim.cmd([[
+  augroup packer_user_config
+  autocmd!
+  autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 local TAB_WIDTH = 4
 vim.bo.tabstop = TAB_WIDTH
 vim.bo.shiftwidth = TAB_WIDTH
