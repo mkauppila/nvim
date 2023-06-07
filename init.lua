481eda3043b46b13b34b628b4115377cb7084f10
Markus Kauppila <markus.kauppila@gmail.com>
2023-06-07 22:18:30 +0300

    Conf basic vim settings

--- init.lua
+++ init.lua
@@ -4,10 +4,36 @@ vim.g.maplocalleader = ' '
 -- note: to fix the icons on macos 
 -- follow this https://stackoverflow.com/q/72899162
 
-local TAB_WIDTH = 4
-vim.bo.tabstop = TAB_WIDTH
-vim.bo.shiftwidth = TAB_WIDTH
-vim.bo.expandtab = true
+-- move to folder vim-options.lua
+local o = vim.opt
+o.relativenumber = true
+o.number = true
+o.tabstop = 2
+o.shiftwidth = 2
+o.expandtab = true
+o.autoindent = true
+o.wrap = false
+o.ignorecase = true
+o.smartcase = true
+o.cursorline = true
+
+o.termguicolors = true
+--  default to darkmode
+o.background = "dark" 
+o.signcolumn = "yes"
+
+-- backspace
+o.backspace = "indent,eol,start"
+
+-- use clipboard when yanking
+o.clipboard:append("unnamedplus")
+
+-- split windows
+o.splitright = true
+o.splitbelow = true
+
+-- makes - part of the word, ie. works better with `dw`
+o.iskeyword:append("-")
 
 
 require('packer-config')
