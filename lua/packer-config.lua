12222bde75af84b47ad88849d66d3987f77d084a
Markus Kauppila <markus.kauppila@gmail.com>
2023-06-09 20:01:29 +0300

    Install surround

--- lua/packer-config.lua
+++ lua/packer-config.lua
@@ -18,6 +18,15 @@ return require('packer').startup(function(use)
 
     use 'mbbill/undotree'
 
+    use({
+      "kylechui/nvim-surround",
+      tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+      config = function()
+        require("nvim-surround").setup({
+          -- Configuration here, or leave empty to use defaults
+        })
+      end
+    })
     use {
       'VonHeikemen/lsp-zero.nvim',
       branch = 'v2.x',
